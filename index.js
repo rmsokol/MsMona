ec73d940819952e70eb66cafd24056300c637cf7
Rachel Sokol <50994670+rmsokol@users.noreply.github.com>
2023-11-29 21:46:15 -0500

    design changes and assets

---
+++ index.js
@@ -0,0 +1,15 @@
+let photos = [
+    appleciderspicecake.png,
+
+    birthdaycake.png,
+
+    birthdaycake2.png,
+
+    bridgertoncake.png,
+
+    hotbutterrumcake.png,
+
+    sweetpotatopie.png,
+
+    xmascookies.png
+]
